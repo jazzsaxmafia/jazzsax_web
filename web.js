316cd653e9b2b69b769861bdea844e2ee1bf7552
unknown <jazzsaxmafia@gmail.com>
2014-08-15 17:26:24 +0900

    0815_3

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var fs = require('fs');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   //:res.send('Hello World!');
-  //res.send("fucking awesome!");
-  res.render('index.html');
+  res.send("fucking awesome!");
+  //res.render('index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
