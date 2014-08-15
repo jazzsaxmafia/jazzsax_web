6a46ad3de74e3ec97b9ed186d4d2337bfff69020
unknown <jazzsaxmafia@gmail.com>
2014-08-16 02:42:43 +0900

    whythefuck

--- web.js
+++ web.js
@@ -12,27 +12,48 @@ app.use(express.static(__dirname+'/public'));
 
 app.use(logfmt.requestLogger());
 
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+							req.query.test === '1';
+	next();
+});
+
 app.get('/', function(req, res) {
   	res.render('home');
 });
 
 app.get('/about', function(req, res){
 	
-	res.render('about', {fortune : fortune.getFortune()});
+	res.render('about', {
+		fortune : fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
 
 app.get('/application', function(req, res){
 	res.render('application');
 });
 
+
 app.get('/contact', function(req, res){
 	res.render('contact');
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
 });
+
+
 
 app.use(function(err, req, res, next){
 	console.error(err.stack);
